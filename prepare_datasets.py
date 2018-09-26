d81c1496fcc7d7844fe0b3bf8457d65d5d5fcac5
DzianisH <gavrilovetsden@gmail.com>
2018-09-27 01:05:48 +0300

    Add translating to source data set to multiply data

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -3,6 +3,7 @@ import tensorflow_hub as hub
 import numpy as np
 import pandas as pd
 from numpy import random
+from googletrans import Translator
 
 gas_sources = ('data/jira-exports/AXNGA.0-4000.980.csv', 'data/jira-exports/AXNGA.4000-8500.965.csv',
                'data/jira-exports/AXNGA.8500-14000.984.csv', 'data/jira-exports/AXNGA.14000-20000.915.csv',
@@ -11,14 +12,13 @@ non_gas_soures = (
     'data/jira-exports/AXNAU.305.csv', 'data/jira-exports/AXNCEE.823.csv', 'data/jira-exports/AXNIN.9.csv',
     'data/jira-exports/GSGN.66.csv')
 
+print('Joining DataSets...')
 
-def shuffle(df: pd.DataFrame, cols=('key', 'time', 'estimate', 'text')) -> pd.DataFrame:
+
+def shuffle(df: pd.DataFrame) -> pd.DataFrame:
     vals = df.values
     random.shuffle(vals)
-    if cols is not None:
-        return pd.DataFrame(columns=cols, data=vals)
-    else:
-        return pd.DataFrame(vals)
+    return pd.DataFrame(columns=df.keys(), data=vals)
 
 
 def read_jiras(paths) -> pd.DataFrame:
@@ -36,13 +36,66 @@ def read_jiras(paths) -> pd.DataFrame:
     return shuffle(df)
 
 
-print('Joining DataSets...')
-
 df_gas = read_jiras(gas_sources)
 df_non_gas = read_jiras(non_gas_soures)
 
 df_gas.to_csv('data/gas.csv')
 df_non_gas.to_csv('data/non_gas.csv')
+
+print('Adding translations...')
+
+def do_translate_chain(text, chain, api):
+    for params in chain:
+        text = api.translate(text, src=params['src'], dest=params['dest']).text
+    return text
+
+def create_with_translation(df: pd.DataFrame):
+    translate_chains = (
+        ({'src': 'de', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'fr'}, {'src': 'fr', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'it'}, {'src': 'it', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'de'}, {'src': 'de', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'es'}, {'src': 'es', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'pt'}, {'src': 'pt', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'nl'}, {'src': 'nl', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'da'}, {'src': 'da', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'no'}, {'src': 'no', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'sv'}, {'src': 'sv', 'dest': 'en'},),
+        ({'src': 'en', 'dest': 'cs'}, {'src': 'cs', 'dest': 'en'},),
+        ({'src': 'de', 'dest': 'en'}, {'src': 'en', 'dest': 'fr'}, {'src': 'fr', 'dest': 'en'},),
+        ({'src': 'de', 'dest': 'en'}, {'src': 'en', 'dest': 'nl'}, {'src': 'nl', 'dest': 'en'},),
+    )
+    df['original'] = True
+    df['lang'] = 'en'
+    translated_rows = []
+    text_pos = df.keys().get_loc('text')
+    original_pos = df.keys().get_loc('original')
+    lang_pos = df.keys().get_loc('lang')
+    api = Translator(timeout=30)
+    for row in df.values:
+        row[original_pos] = False
+        src_text = row[text_pos]
+        translated_texts = {src_text}
+        for chain in translate_chains:
+            # print('creating translations for chain', chain)
+            new_text = do_translate_chain(src_text, api=api, chain=chain)
+            if not translated_texts.__contains__(new_text):
+                translated_texts.add(new_text)
+                new_row = row[:]
+                new_row[text_pos] = new_text[:]
+                new_row[lang_pos] = chain[:]
+                translated_rows.append(new_row)
+    return df.append(pd.DataFrame(translated_rows, columns=df.keys()))
+
+print('adding translations for non gas')
+df_non_gas = create_with_translation(df_non_gas)
+print('adding translations for gas')
+df_gas = create_with_translation(df_gas)
+
+df_gas.to_csv('data/gas-with-translations.csv')
+df_non_gas.to_csv('data/non-gas-with-translations.csv')
+
+print('Splitting DataSets...')
 
 count_ng = df_non_gas.shape[0]
 count_g = df_gas.shape[0]
@@ -53,8 +106,6 @@ train_sum = train_g + count_ng
 
 print('GAS items:', count_g, 'non GAS items:', count_ng, 'sum:', sum)
 print('Test:', test, 'Train:', train_sum, 'Train GAS:', train_g, 'GAS Train %:', train_g / train_sum)
-
-print('Splitting DataSets...')
 
 df_test = df_gas[:test]
 df_train = df_non_gas.append(df_gas[test:], ignore_index=True)
@@ -70,9 +121,11 @@ with tf.Graph().as_default():
     module = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2", trainable=False)
     embeddings = module(sentences)
 
+
     def embed_datasets(sets):
         sess = tf.train.MonitoredSession()
         return (embed_dataset(ds, sess) for ds in sets)
+
 
     def embed_dataset(ds: pd.DataFrame, sess):
         step = 128
@@ -85,6 +138,7 @@ with tf.Graph().as_default():
             embeds = np.append(embeds, chunk, 0)
             print(embeds.shape)
         return {'embeddings': embeds}
+
 
     print('preparing datasets')
     train, test = embed_datasets((df_train, df_test))
--- workspace_setup.sh
+++ workspace_setup.sh
@@ -2,5 +2,7 @@ pip3 install pandas
 pip3 install numpy
 pop3 install tensorflow
 pop3 install tensorflow-hub
+# https://stackoverflow.com/questions/52455774/googletrans-stopped-working-with-error-nonetype-object-has-no-attribute-group
+pip3 install googletrans
 
 export TFHUB_CACHE_DIR models_cache
