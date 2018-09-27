c22da7da82b672266f4d72d87875fd35412a92ee
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2018-09-27 17:54:55 +0300

    Fix setup script, improve translations, add more datasets

--- workspace_setup.sh
+++ workspace_setup.sh
@@ -1,8 +1,13 @@
 pip3 install pandas
 pip3 install numpy
-pop3 install tensorflow
-pop3 install tensorflow-hub
+pip3 install tensorflow
+pip3 install tensorflow-hub
 # https://stackoverflow.com/questions/52455774/googletrans-stopped-working-with-error-nonetype-object-has-no-attribute-group
-pip3 install googletrans
+# pip3 install googletrans
+git clone https://github.com/BoseCorp/py-googletrans.git
+cd ./py-googletrans
+python3 setup.py install
+cd ..
 
-export TFHUB_CACHE_DIR models_cache
+export TFHUB_CACHE_DIR=models_cache
+mkdir $TFHUB_CACHE_DIR
