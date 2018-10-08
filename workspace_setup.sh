1c4322a01df301c5b5ff79e6444f528633a270ce
DzianisH <gavrilovetsden@gmail.com>
2018-10-08 13:34:27 +0300

    Update workspace_setup.sh

--- workspace_setup.sh
+++ workspace_setup.sh
@@ -1,8 +1,4 @@
-pip3 install pandas
-pip3 install numpy
-pip3 install tensorflow
-pip3 install tensorflow-hub
-pip3 install yandex_translate
+pip3 install pandas numpy tensorflow tensorflow-hub yandex_translate
 
 export TFHUB_CACHE_DIR=models_cache
 mkdir $TFHUB_CACHE_DIR
